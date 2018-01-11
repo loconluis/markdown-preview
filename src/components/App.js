edef43aaedca9b3c24a900f57e232affc7c05b52
loconluis <luis.locon@gmail.com>
2018-01-11 17:33:24 -0600

    First commit for our preview markdown editor

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+// Components
+import MainHeader from './MainHeader'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MainHeader />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/MainHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Markdown Preview</h1>
+      </header>
+    </div>
+  )
+}
