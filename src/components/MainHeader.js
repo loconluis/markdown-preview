ce2d016bb6d7079a11cdccc22d13b6cd15954e49
loconluis <luis.locon@gmail.com>
2018-01-12 00:29:15 -0600

    Fixing some styles

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => {
   return (
     <div className="header">
-        <h1 className="App-title">Markdown Preview</h1>
+        <h1 className="header__title">Markdown Preview</h1>
     </div>
   )
 }
