4954f94c9ca78167330550fbec6d80d5332424a5
loconluis <luis.locon@gmail.com>
2018-01-11 22:30:19 -0600

    Adding stufss

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => {
   return (
-    <div className="App">
+    <div className="header">
       <header className="App-header">
         <h1 className="App-title">Markdown Preview</h1>
       </header>
